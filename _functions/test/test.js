92079c00b41d1855791deb4dce673c72a7563bbe
Niels <45306483+UltraNiels@users.noreply.github.com>
2019-12-29 18:28:19 +0100

    Update test.js

--- _functions/test/test.js
+++ _functions/test/test.js
@@ -2,7 +2,6 @@ exports.handler = function(event, context, callback) {
     // your server-side functionality
     callback(null, {
     	statusCode: 200,
-    	body: `The test tested succelsfully test-like. \n
-    	Randomness: ${Math.floor(Math.random() * 100)}`
+    	body: 'The test tested succelsfully test-like. \n\n' + `Randomness: ${Math.floor(Math.random() * 100)}`
     });
 }
